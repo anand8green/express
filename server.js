32f5f264451e1f0c7d3d64785bd91c716a846bd2
Anand Kumar <anandkumar@Anands-iMac.local>
2021-03-10 13:53:55 +0000

    add

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const app = express()
 
 app.use(cors())
 
-app.get("/", (req, res) => {
-    res.send("Hello World")
-})
+// app.get("/", (req, res) => {
+//     res.send("Hello World")
+// })
 
-
+app.use(express.static(__dirname + "/static"));
 
 app.listen(process.env.PORT || 1000, () => {
     console.log("server running")
